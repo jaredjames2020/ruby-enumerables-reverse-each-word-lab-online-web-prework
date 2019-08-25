b95a8fb973fd51d71e30c97c9097dd1f368bb064
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-25 19:43:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,24 +7,14 @@ def reverse_each_word(sentence)
   new_array = []
   
   sentence.each do |x|
-    sentence.split(/ /)
+    #sentence.split(/ /)
     new_array << x.reverse
-    
+  end
+  new_array
     
   
-  sentence.split(/ /)
-  new_array2.each {|x| print "#{x.reverse}"}
-  #new_array2 = sentence.split(/ /)
-  #new_array = new_array2.join(" ")
-  #p new_array.reverse
-
-  #if i < sentence.split(/ /).length
-   # sentence.each {|x| new_array2.push(x.reverse)}
-    #i += 1
-    #p new_array2
-  
-  #new_array = sentence.split(/ /)
-  #new_array.each {|x| p x.reverse}.join(" ")
+  #sentence.split(/ /)
+  #new_array2.each {|x| print "#{x.reverse}"}
     end
 reverse_each_word("Hello there, and how are you?")
 
