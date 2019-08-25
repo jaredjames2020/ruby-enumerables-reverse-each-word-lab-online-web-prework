0d20c804e5ebad3e56a9668e8467ad8c3e664e75
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-25 19:49:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,10 @@ def reverse_each_word(sentence)
     end
     new_array.join(" ")
 end
-#reverse_each_word("Hello there, and how are you?")
+#reverse_each_word("Hello there, and how are you?") #test code line
 
 def reverse_each_word(sentence)
   new_array = sentence.split(/ /)
   new_array.collect {|x| p x.reverse}.join(" ")
 end
-#reverse_each_word("Hello there, and how are you?")
+#reverse_each_word("Hello there, and how are you?") #test code line
